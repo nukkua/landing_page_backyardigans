2ed9ccf97999d82cd511687a85afc063e7e8e1e4
Chesco216 <franchescofv198@gmail.com>
2024-08-11 13:07:41 -0400

    astra zzz (no se usar astro xd)

---
+++ src/components/team/Team.js
@@ -0,0 +1,38 @@
+export const team = [
+  {
+    imgURL: 'https://cdn-icons-png.flaticon.com/512/4042/4042356.png',
+    desc: 'Dirijo la estrategia general de la empresa, asegurándome de que todos los departamentos trabajen juntos para alcanzar nuestros objetivos a largo plazo.',
+    name: 'Zebra',
+    position: 'Director ejecutivo'
+  },
+  {
+    imgURL: 'https://cdn-icons-png.flaticon.com/512/4042/4042356.png',
+    desc: 'Superviso la eficiencia diaria de la empresa, optimizando recursos y mejorando procesos para cumplir con nuestras metas estratégicas.',
+    name: 'Zebra',
+    position: 'Director de operaciones'
+  },
+  {
+    imgURL: 'https://cdn-icons-png.flaticon.com/512/4042/4042356.png',
+    desc: 'Me encargo de maximizar los ingresos, alineando las estrategias de ventas, marketing y atención al cliente para impulsar el crecimiento.',
+    name: 'Zebra',
+    position: 'Director de ingresos'
+  },
+  {
+    imgURL: 'https://cdn-icons-png.flaticon.com/512/4042/4042356.png',
+    desc: 'Innvo tecnologicamente nuestra empresa, asegurando que nuestra infraestructura sea segura, escalable y competitiva.',
+    name: 'Zebra',
+    position: 'Director de tecnologia'
+  },
+  {
+    imgURL: 'https://cdn-icons-png.flaticon.com/512/4042/4042356.png',
+    desc: 'Desarrollo el talento y lidero la transformación organizacional, creando un entorno que fomente el crecimiento y la innovación.',
+    name: 'Zebra',
+    position: 'Director de talentos y transformacion'
+  },
+  {
+    imgURL: 'https://cdn-icons-png.flaticon.com/512/4042/4042356.png',
+    desc: 'Me aseguro de que nuestras operaciones cumplan con las normativas y que estemos bien posicionados en los mercados en los que operamos.',
+    name: 'Zebra',
+    position: 'Vicepresidente de mercados y regulacion'
+  },
+]
